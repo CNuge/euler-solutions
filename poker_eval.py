a2b37e0afc1779cbf02bd9bb15ce1cae66437fc9
WATTBA <nugentc@uoguelph.ca>
2018-04-14 12:58:27 -0400

    bug search

--- poker_eval.py
+++ poker_eval.py
@@ -37,10 +37,6 @@ https://projecteuler.net/problem=54
 """
 
 
-ex_hands = ['8C TS KC 9H 4S', '7D 2S 5D 3S AC', '5C AD 5D AC 9C' ,
-			'7C 5H 8D TD KS', '3H 7H 6S KC JS' ,'QH TD JC 2D 8S' ,
-			'QD AS 6H JS 2C' , '3D 9H KC 4H 8S', 'KD 8S 9S 7C 2S' ,
-			'3S 6D 6S 4H KC']
 
 class PokerHand():
 	def __init__(self, card_string):
@@ -56,7 +52,7 @@ class PokerHand():
 							for x in card_string.split()]
 
 		#the face values, for rank comparison
-		self.face_vals = sorted([x[0] for x in self.cards])[::-1]
+		self.face_dats = sorted([x[0] for x in self.cards])[::-1]
 
 		#the count of each card, 
 		self.val_count = { k:0 for k in self.face_dict.values()}
@@ -69,8 +65,8 @@ class PokerHand():
 
 		self.score_hand()
 
-	def is_straight(self):
-		if self.face_vals[4] ==  (self.face_vals[0] - 4):
+	def is_straight(self):		
+		if self.face_dats[4] == (self.face_dats[0] - 4) and len(set(self.face_dats)) == 5:
 			self.straight = 1
 		else:
 			self.straight = 0
@@ -109,28 +105,155 @@ class PokerHand():
 					self.quad,
 					self.triple,
 					self.doubles,
-					self.face_vals)
+					self.face_dats)
 
-filename='poker.txt'
 
-def game(filename):
+def high_card(p1_dat, p2_dat):
+	print(p1_dat, p2_dat)
+	""" determine winner based on who has the higher card"""
+	while len(p1_dat) > 0 and len(p2_dat) > 0:
+		if p1_dat[0] == p2_dat[0]:
+			p1_dat.pop(0)
+			p2_dat.pop(0)
+		elif p1_dat[0] > p2_dat[0]:
+			return (1,0)
+		elif p1_dat[0] < p2_dat[0]:
+			return (0,1)
+	return(0,0)
+
+def compare_hands(p1, p2):
+	""" take in two PokerHand class instances and determine the winner
+		output (1,0) is p1 wins and (0,1) if p2 wins """
+	p1_win = (1,0)
+	p2_win = (0,1)
+	p1_dat = p1.components
+	p2_dat = p2.components
+
+	# two straight flushes, 
+	if p1_dat[0] == 1 and p1_dat[1] == 1 and p2_dat[0] == 1 and p2_dat[1] == 1 :
+		return high_card(p1_dat[-1], p2_dat[-1])
+
+	# straight flush p1
+	elif  p1_dat[0] == 1 and p1_dat[1] == 1 :
+		return p1_win
+
+	elif p2_dat[0] == 1 and p2_dat[1] == 1 :
+		return p2_win
+
+	#check for quads
+	elif p1_dat[2] != 0 and p2_dat[2] != 1 :
+		return high_card(p1_dat[-1], p2_dat[-1])
+
+	elif p1_dat[2] != 0 :
+		return p1_win
+
+	elif p2_dat[2] != 0 :
+		return p2_win
+
+	#full house
+	elif p1_dat[3] != 0 and len(p1_dat[4]) != 0 and p2_dat[3] != 0 and len(p2_dat[4]) != 0 :
+		#cant have matching triples on double full house, so only check triples
+		if p1_dat[3] > p2_dat[3]:
+			return p1_win
+		elif p1_dat[3] < p2_dat[3]:
+			return p2_win
+	
+	elif p1_dat[3] != 0 and len(p1_dat[4]) != 0 :
+		return p1_win
+
+	elif p2_dat[3] != 0 and len(p2_dat[4]) != 0 :
+		return p2_win
+
+	#flush
+	elif p1_dat[0] == 1  and p2_dat[0] == 1  :
+		return high_card(p1_dat[-1], p2_dat[-1])
+
+	elif p1_dat[0] == 1 :
+		return p1_win
+
+	elif p2_dat[0] == 1 :
+		return p2_win
+
+	#straight
+	elif p1_dat[1] == 1 and p2_dat[1] == 1 :
+		return high_card(p1_dat[-1], p2_dat[-1])
+
+	elif p1_dat[1] == 1 :
+		return p1_win
+
+	elif p2_dat[1] == 1 :
+		return p2_win
+
+	#3 of a kind
+	elif p1_dat[3] > p2_dat[3]:
+		return p1_win
+	elif p1_dat[3] < p2_dat[3]:
+			return p2_win
+	
+	#two pairs
+	elif len(p1_dat[4]) > 1 and len(p2_dat[4]) > 1:
+		if p1_dat[4][0] == p2_dat[4][0]:
+			if p1_dat[4][1] == p2_dat[4][1]:
+				return high_card(p1_dat[-1], p2_dat[-1])
+			
+			elif p1_dat[4][1] > p2_dat[4][1]:
+				return p1_win
+			
+			elif p1_dat[4][1] < p2_dat[4][1]:
+				return p2_win
+
+		elif p1_dat[4][0] > p2_dat[4][0]:
+			return p1_win
+		
+		elif p1_dat[4][0] < p2_dat[4][0]:
+			return p2_win
+	
+	#pairs
+	elif len(p1_dat[4]) == 1 and len(p2_dat[4]) == 1:
+		if p1_dat[4][0] == p2_dat[4][0]:
+			return high_card(p1_dat[-1], p2_dat[-1])
+
+		elif p1_dat[4][0] > p2_dat[4][0]:
+			return p1_win
+		
+		elif p1_dat[4][0] < p2_dat[4][0]:
+			return p2_win
+		
+	#high card
+	else:
+		return high_card(p1_dat[-1], p2_dat[-1])
+
+def poker_game(filename):
 	p1_wins = 0
 	p2_wins = 0
 
 	with open(filename) as file:
-		for line in file:
+		for i, line in enumerate(file):
+
 			dat = line.rstrip()
 			p1 = PokerHand(dat[:14])
 			p2 = PokerHand(dat[15:])
+			print(i)
+			print(p1.components, p2.components)
+
+			winner = compare_hands(p1, p2)
+			p1_wins += winner[0]
+			p2_wins += winner[1]
+
+	return f"player 1 wins: {p1_wins}, player 2 wins: {p2_wins}.\n"
 
 
-x = PokerHand('8C TS KC 9H 4S')
-x.cards
-x.components
+
+ex_hands = ['8C TS KC 9H 4S', '7D 2S 5D 3S AC', '5C AD 5D AC 9C' ,
+			'7C 5H 8D TD KS', '3H 7H 6S KC JS' ,'QH TD JC 2D 8S' ,
+			'QD AS 6H JS 2C' , '3D 9H KC 4H 8S', 'KD 8S 9S 7C 2S' ,
+			'3S 6D 6S 4H KC']
 
 
+filename='poker.txt'
 
 
+poker_game(filename)
 
 
 
