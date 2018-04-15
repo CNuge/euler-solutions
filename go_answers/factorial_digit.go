a0b5940fda196d53e7f90b6289295168e27ea339
WATTBA <nugentc@uoguelph.ca>
2018-04-15 11:42:06 -0400

    fit

--- go_answers/factorial_digit.go
+++ go_answers/factorial_digit.go
@@ -4,7 +4,7 @@ n! means n × (n − 1) × ... × 3 × 2 × 1
 
 For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
 
-and the sum of the digits in the number 10! 
+and the sum of the digits in the number 10!
 is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
 Find the sum of the digits in the number 100!
@@ -21,17 +21,15 @@ import (
 	"math/big"
 )
 
-
 func Factorial(number int) big.Int {
-	output :=  big.NewInt(0)
+	output := big.NewInt(0)
 
-	output.MulRange( int64(1), int64(number))
+	output.MulRange(int64(1), int64(number))
 
 	return *output
 }
 
-
-func BigDigitSum(number big.Int) int{
+func BigDigitSum(number big.Int) int {
 	total := 0
 
 	for _, i := range number.String() {
@@ -41,14 +39,12 @@ func BigDigitSum(number big.Int) int{
 	return total
 }
 
+func main() {
 
-func main(){
- 
 	fact_total := Factorial(100)
 	fmt.Println(fact_total.String())
 
 	fact_digit_total := BigDigitSum(fact_total)
 	fmt.Println(fact_digit_total)
-
 
 }
