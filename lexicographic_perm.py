42787b36fbdca5e462260c49b6db20dd80b0082a
WATTBA <nugentc@uoguelph.ca>
2018-04-18 10:41:01 -0400

    k.i.s.s. solution

--- lexicographic_perm.py
+++ lexicographic_perm.py
@@ -15,3 +15,15 @@ What is the millionth lexicographic permutation of the digits
 https://projecteuler.net/problem=24
 
 """
+
+from itertools import permutations
+
+digits = ['0','1','2','3','4','5','6','7','8','9',]
+
+all_perms = [''.join(x) for x in permutations(digits)]
+
+len(all_perms)
+
+sorted_perms = sorted(all_perms)
+
+sorted_perms[999999]
