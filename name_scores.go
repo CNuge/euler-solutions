427fec15fdfad421d7965f4bc75b6a99a6d7ce1a
WATTBA <nugentc@uoguelph.ca>
2018-04-17 20:59:04 -0400

    working

--- name_scores.go
+++ name_scores.go
@@ -32,7 +32,7 @@ import(
 
 	// map to get the value a = 1, b = 2
 
-func NameScore(name string) int{
+func NameScore(name string) int {
 	var letter_map = map[rune]int{
 		'A':1, 'B':2, 'C':3, 'D':4, 'E': 5, 'F': 6, 'G': 7, 'H':8, 'I':9, 'J':10,
 		'K': 11 , 'L': 12, 'M':13, 'N':14, 'O': 15, 'P':16, 'Q': 17, 'R': 18, 'S':19,
@@ -51,8 +51,12 @@ func NameScore(name string) int{
 func NameScoresTotal(name_list []string) int{
 	total := 0
 
-	letter
-
+	for i, name := range name_list{
+		n_score :=  NameScore(name)
+		n_total := (i+1) * n_score
+		total += n_total
+	}
+	return total
 }
 
 func main(){
@@ -70,5 +74,4 @@ func main(){
 	output := NameScoresTotal(name_data)
 	fmt.Println(output)
 
-	fmt.Println(NameScore("CAM", 1))
 }
