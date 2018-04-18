db532caca9bc6ed8613afb1feafaa5a7ab09873e
WATTBA <nugentc@uoguelph.ca>
2018-04-18 10:12:42 -0400

    lexi

--- go_answers/name_scores.go
+++ go_answers/name_scores.go
@@ -27,9 +27,7 @@ import (
 )
 
 // determine name score
-
 // map to get the value a = 1, b = 2
-
 func NameScore(name string) int {
 	var letter_map = map[rune]int{
 		'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10,
