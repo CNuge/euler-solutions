98cacc4eeac03c1d315449c9d3f2d03be91166dc
CNuge <nugentc@uoguelph.ca>
2018-05-04 09:43:41 -0400

    prime sum, new gh desktop test

--- consecutive_prime_sum.py
+++ consecutive_prime_sum.py
@@ -1,3 +1,4 @@
+"""
 The prime 41, can be written as the sum of six consecutive primes:
 
 41 = 2 + 3 + 5 + 7 + 11 + 13
@@ -7,6 +8,8 @@ The longest sum of consecutive primes below one-thousand that adds to a prime, c
 
 Which prime, below one-million, can be written as the sum of the most consecutive primes?
 
-
-
 https://projecteuler.net/problem=50
+
+"""
+
+
