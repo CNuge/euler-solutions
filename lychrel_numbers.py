458893cdfbc7f4019c4615993d1228ddc9b90e42
CNuge <nugentc@uoguelph.ca>
2018-05-07 07:32:27 -0400

    lychrel

--- lychrel_numbers.py
+++ lychrel_numbers.py
@@ -1,3 +1,4 @@
+""""
 If we take 47, reverse and add, 47 + 74 = 121, which is palindromic.
 
 Not all numbers produce palindromes so quickly. For example,
@@ -29,5 +30,41 @@ NOTE: Wording was modified slightly on 24 April 2007 to emphasise the theoretica
 nature of Lychrel numbers.
 
 
-
 https://projecteuler.net/problem=55
+"""
+
+
+def is_palindrome(number):
+	if str(number) == str(number)[::-1]:
+		return True
+
+def reverse_number(number):
+	return int(str(number)[::-1])
+
+
+def forms_palindrome(number, cap):
+	iteration = 0
+
+	while iteration < cap:
+		rev = reverse_number(number)
+		number = number + rev
+		if is_palindrome(number):
+			return True
+
+		iteration += 1
+
+	return False
+
+if __name__ == '__main__':
+	""" A number that never forms a palindrome through the 
+	reverse and add process is called a Lychrel number. """
+
+	lychrel_count = 0
+
+	for i in range(1,10000):
+		if forms_palindrome(i , 50) == False:
+			lychrel_count += 1
+
+	print(f'There are {lychrel_count} Lychrel numbers between 1 and 10000')
+
+
