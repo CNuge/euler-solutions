7f2df9c7672d4240b308c2f2dea4f54834b7ac20
CNuge <nugentc@uoguelph.ca>
2018-05-07 12:11:25 -0400

    replacement scap multiple locations

--- prime_replacement.py
+++ prime_replacement.py
@@ -15,7 +15,7 @@ with the same digit, is part of an eight prime value family.
 https://projecteuler.net/problem=51
 """
 
-
+from itertools import permutations
 
 def is_prime(number):
 	""" take a number and return a boolean indicating
@@ -32,7 +32,59 @@ def is_prime(number):
 
 
 def replacement_scan(number, prime_set):
-	""" iteratively replace digits in a number and find a prime set equal to the length of the query""" 
+	""" replace components of the number, to check for prime permutations"""
+	digit_dict = {k: int(v) for k, v in enumerate(str(number))}
+
+	combos = []
+	for n in range(2, len(digit_dict.keys())+1):
+    	for perm in permutations(digit_dict.keys(), n):
+    		if sorted(perm) not in combos and sorted(perm)[0] != 0:
+	    		combos.append(sorted(perm))
+
+	for combo in combos:
+		prime_list = []
+	    for digit in range(0,10):
+	    	test_dict = dict(digit_dict)
+	    	for pos in combo:
+	    		test_dict[pos] = digit
+    		new_dig = int(''.join([str(x) for x in test_dict.values()]))
+    		
+    		if is_prime(new_dig):
+    			prime_list.append(new_dig)
+
+	    out_list = list(set(prime_list))
+		
+		#below we use set, because the scan will record the initial number t
+		if len(prime_list)== prime_set:
+			return True, sorted(prime_list)
+	return False, 0
+
+
+
+if __name__ == '__main__':
+
+replacement_scan(13, 6)
+
+replacement_scan(56003, 7)
+replacement_scan(56773, 7)
+
+ans = 0
+for i in range(56773, 1000000, 2):
+	if is_prime(i):
+		print(i)
+		output = replacement_scan(i, 8)
+		if output[0] == True:
+			print(output[1])
+			ans = output[1][0]
+			break
+
+print(ans)
+
+
+
+"""
+def replacement_scan_two_spots(number, prime_set):
+	#iteratively replace digits in a number and find a prime set equal to the length of the query 
 	num_string = str(number)
 
 	for i, x1 in enumerate(num_string):
@@ -53,27 +105,7 @@ def replacement_scan(number, prime_set):
 				if len(prime_list)== prime_set:
 					return True, sorted(prime_list)
 	return False, 0
-
-
-
-
-
-if __name__ == '__main__':
-
-replacement_scan(13, 6)
-
-replacement_scan(56003, 7)
-
-for i in range(56993, 1000000, 2):
-	if is_prime(i):
-		output = replacement_scan(i, 8)
-		if output[0] == True:
-			print(output[0])
-			break
-
-
-
-
+"""
 
 
 
