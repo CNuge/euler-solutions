f2edce27891ea0f2f1841c8f2067df0d6dedc74a
CNuge <nugentc@uoguelph.ca>
2018-05-08 11:11:07 -0400

    interesting lesson

--- sqrt_convergents.py
+++ sqrt_convergents.py
@@ -1,4 +1,10 @@
 """
+My initial solution for this was very literal... and wrong as a result.
+I left it commented out at the bottom as it is a good reminder of how I
+incorrectly approached the problem... I then looked up the proper formula
+for the calculation of the next numerator and denominator (missed this pattern)
+and then was able to arrive at the proper solution.
+
 It is possible to show that the square root of two can be expressed 
 as an infinite continued fraction.
 
@@ -23,46 +29,33 @@ https://projecteuler.net/problem=57
 """
 
 from fractions import Fraction
+from math import sqrt
 
 
-def find_last_2(formula):
-	""" search the formula string and return the index position that contains the
-		last '2' in the formual """
-	for i , digit in enumerate(formula[::-1]):
-		if digit == '2':
-			return -(1+i)
+def next_fraction(old_num, old_denom):
+	""" determine the square root of two for the nth iteration of the 
+		repeating fraction"""
 
+	new_num = old_num + (2*old_denom)
+	new_denom = old_num + old_denom
 
-def continued_fraction( formula ):
-	"""take a string representing the sqrt(2) as a recurring fraction and
-		provide the next occurance by replacing the trailing denominator"""
-
-	new_denom = '(2 + 1/2)'
-
-	last_2 = find_last_2(formula)
-
-	new_formula = formula[:last_2] + new_denom + formula[last_2+1:]
-
-	return new_formula
-
+	return new_num, new_denom
 
 
 def iterate_sqrt_2(max_iter):
 	count = 0
-
 	iteration = 1
-	formula = '1 + (1/2)'
+	
+	num = 3
+	denom = 2
 
 	while iteration < max_iter :
 
 		iteration += 1
+		
+		num, denom = next_fraction(num, denom)
 
-		formula = continued_fraction(formula)
-		value = eval(formula)
-
-		vf = Fraction(value).limit_denominator(10000)
-
-		if len(str(vf.numerator)) > len(str(vf.denominator)):
+		if len(str(num)) > len(str(denom)):
 			count += 1
 
 	return count
@@ -70,9 +63,8 @@ def iterate_sqrt_2(max_iter):
 
 if __name__ == '__main__':
 
-	eval('1 + 1/2')
 
-	continued_fraction('1 + 1/(2 + 1/2)') == '1 + 1/(2 + 1/(2 + 1/2))'
+	next_fraction(3, 2)
 
 	ans = iterate_sqrt_2(1000)
 
@@ -83,3 +75,29 @@ if __name__ == '__main__':
 
 
 	Fraction(1.4).limit_denominator(10000)
+
+
+
+
+"""
+def find_last_2(formula):
+	# search the formula string and return the index position that contains the
+	#	last '2' in the formual
+	for i , digit in enumerate(formula[::-1]):
+		if digit == '2':
+			return -(1+i)
+
+
+def continued_fraction( formula ):
+	#take a string representing the sqrt(2) as a recurring fraction and
+	#	provide the next occurance by replacing the trailing denominator
+
+	new_denom = '(2 + 1/2)'
+
+	last_2 = find_last_2(formula)
+
+	new_formula = formula[:last_2] + new_denom + formula[last_2+1:]
+
+	return new_formula
+
+"""
