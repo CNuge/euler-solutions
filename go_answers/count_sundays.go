42cf1a84f4bbf428e73b072ca1547b20ab090454
CNuge <nugentc@uoguelph.ca>
2018-08-20 08:15:35 -0400

    go sunday count

---
+++ go_answers/count_sundays.go
@@ -0,0 +1,93 @@
+/*
+You are given the following information, but you may prefer to do some research for yourself.
+
+1 Jan 1900 was a Monday.
+Thirty days has September,
+April, June and November.
+All the rest have thirty-one,
+Saving February alone,
+Which has twenty-eight, rain or shine.
+And on leap years, twenty-nine.
+A leap year occurs on any year evenly divisible by 4, but not on a 
+century unless it is divisible by 400.
+How many Sundays fell on the first of the month during the twentieth 
+century (1 Jan 1901 to 31 Dec 2000)?
+
+https://projecteuler.net/problem=19
+*/
+
+package main
+
+import(
+	"fmt"
+)
+
+
+
+type DateTime struct{
+	day_of_week int
+	day			int
+	month		int
+	year		int
+}
+
+
+
+func count_sundays(dt DateTime, finish_year int) int {
+	var month_map = map[int]int{
+		1 : 31,
+		2 : 28,
+		3 : 31,
+		4 : 30,
+		5 : 31,
+		6 : 30,
+		7 : 31,
+		8 : 31,
+		9 : 30, 
+		10 : 31,
+		11 : 30,
+		12 : 31}
+
+	total_sundays := 0 
+
+	for ; ; dt['year'] < finish_year + 1{
+		// check if the day is a sunday
+		if dt['day_of_week'] == 7{
+			// check if its the first of the month
+			if dt['day'] == 1 {
+				total_sundays += 1
+			}
+			// reset day of week
+			dt['day_of_week'] = 1
+		}else{
+			//incrementing day of wee
+			dt['day_of_week'] += 1
+		}
+		// check if we need to roll over the month or the year, act accordingly
+		if dt['day'] == month_map[dt['month']]{
+			// leap year fringe case
+			if (dt['month'] == 2) && (dt['year'] % 4 == 0){
+				// make sure its not a year divisible by 400
+			}
+
+		}
+	}
+
+}
+
+
+
+func main(){
+
+	start_date = DateTime{
+		day_of_week: 1,
+		day:		1,
+		month:	 	1,
+		year:		1901,
+	}
+
+	fmt.Println(count_sundays(start_date, 2000))
+
+}
+
+
